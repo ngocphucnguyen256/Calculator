8f60f7a3d976397560c1c68e72555b2258253edf
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-06-12 16:06:36 +0700

    eval?

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,10 +20,15 @@ import "../componentsCss/Calculator.css"
             case 'equal':
                 this.calculate(); 
                 break;
+            case 'backspace':
+                var slice=  this.state.data.slice(0,-1);
+                this.setState({data : slice});
+                break;
             default:
                 this.setState({data: this.state.data + value});
          }
      }
+     
      calculate =()=>{
          try{
              const result = eval(this.state.data);
@@ -37,28 +42,48 @@ import "../componentsCss/Calculator.css"
                 <div className="calculator">
                     <Display data={this.state.data}/>
                     <Keypad>
-                        
-                        <Button  onClick={this.handleClick} label="C" value="clear"/>
-                        <Button onClick={this.handleClick}    label="7" value="7"/>
-                        <Button onClick={this.handleClick} label="4" value="4"/>
-                        <Button  onClick={this.handleClick}   label="1" value="1"/>
+                    <Button  onClick={this.handleClick} label="C" value="clear"/>
+                    <Button   onClick={this.handleClick}  label="<=" value="backspace"/>
+                    <Button onClick={this.handleClick} label="^" value="^"/>
+                    <Button   onClick={this.handleClick}  label="/" value="/"/>
 
-                        <Button   onClick={this.handleClick}  label="/" value="/"/>
-                        <Button  onClick={this.handleClick}   label="8" value="8"/>
-                        <Button  onClick={this.handleClick}   label="5" value="5"/>
-                        <Button  onClick={this.handleClick}   label="2" value="2"/>
-                        <Button   onClick={this.handleClick}  label="." value="."/>
 
-                        <Button onClick={this.handleClick} label="x" value="x"/>
-                        <Button   onClick={this.handleClick}  label="9" value="9"/>
-                        <Button onClick={this.handleClick} label="6" value="6"/>
-                        <Button   onClick={this.handleClick}  label="3" value="3"/>
-                        <Button  onClick={this.handleClick}   label="0" value="0"/>
-
-                        <Button   onClick={this.handleClick}  label="-" value="-"/>
-                        <Button onClick={this.handleClick} label="+" value="+" size="2"/>
-                        <Button   onClick={this.handleClick}  label="=" value="equal" size="2"/>
                     </Keypad>
+                    <Keypad>
+                    <Button onClick={this.handleClick}    label="7" value="7"/>
+                    <Button  onClick={this.handleClick}   label="8" value="8"/>
+                    <Button   onClick={this.handleClick}  label="9" value="9"/>
+                    <Button onClick={this.handleClick} label="x" value="x"/>
+
+
+                    </Keypad>
+                    <Keypad>
+                    <Button onClick={this.handleClick} label="4" value="4"/>
+                    <Button  onClick={this.handleClick}   label="5" value="5"/>
+                    <Button onClick={this.handleClick} label="6" value="6"/>
+                    <Button   onClick={this.handleClick}  label="-" value="-"/>
+
+
+                    </Keypad>
+                    <Keypad>
+                    <Button  onClick={this.handleClick}   label="1" value="1"/>
+                    <Button  onClick={this.handleClick}   label="2" value="2"/>
+                    <Button   onClick={this.handleClick}  label="3" value="3"/>
+
+                    <Button onClick={this.handleClick} label="+" value="+"/>
+
+                    </Keypad>
+                    <Keypad>
+                    <Button   onClick={this.handleClick}  label="." value="."/>
+                    <Button  onClick={this.handleClick}   label="0" value="0"/>
+                    <Button onClick={this.handleClick} label="mod" value="%"/>
+
+                    <Button   onClick={this.handleClick}  label="=" value="equal"/>
+ 
+                    </Keypad>
+
+
+       
                 </div>
             )
         }
