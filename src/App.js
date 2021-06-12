22d52e668179471089f753f19d1c43d1709d5513
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-06-12 23:53:02 +0700

    Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {Route, Switch} from "react-router-dom";
 import Calculator from'./components/Calculator.js';
-function App() {
-  return (
-    <Calculator/>
-  );
+import React  from 'react';
+class  App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/">
+           <Calculator/>
+        </Route>
+      </Switch>
+ 
+    );
+  }
+
 }
 
 export default App;
